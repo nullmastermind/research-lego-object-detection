849f3cf0216a2e7a87a476e1265c4e8e31a45fc0
bluevariant <0x317374@gmail.com>
2021-01-04 21:39:26 +0700

    refactor(Preprocessing): Background removal.

--- preprocessing.py
+++ preprocessing.py
@@ -2,98 +2,59 @@ import cv2
 import numpy as np
 
 
-def shadow_remover():
-    # read an image with shadow...
-    # and it converts to BGR color space automatically
-    or_img = cv2.imread("shadow.png")
+def background_removal(img):
+    # convert to gray
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # blur
+    smooth = cv2.GaussianBlur(gray, (95, 95), 0)
+    # divide gray by morphology image
+    division = cv2.divide(gray, smooth, scale=220)
+    img = cv2.cvtColor(division, cv2.COLOR_GRAY2BGR)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(img, (9, 9), 0)
+    # Edge detection
+    edges = cv2.Canny(gray, 10, 90)
+    edges = cv2.dilate(edges, None)
+    edges = cv2.erode(edges, None)
+    # Find contours in edges, sort by area
+    contour_info = []
+    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    # covert the BGR image to an YCbCr image
-    y_cb_cr_img = cv2.cvtColor(or_img, cv2.COLOR_BGR2YCrCb)
+    for c in contours:
+        contour_info.append(
+            (
+                c,
+                cv2.isContourConvex(c),
+                cv2.contourArea(c),
+            )
+        )
 
-    # copy the image to create a binary mask later
-    binary_mask = np.copy(y_cb_cr_img)
+    contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
+    # max_contour = contour_info[0]
+    # Create empty mask and flood fill
+    mask = np.zeros(edges.shape)
 
-    # get mean value of the pixels in Y plane
-    y_mean = np.mean(cv2.split(y_cb_cr_img)[0])
+    for c in contour_info:
+        cv2.fillConvexPoly(mask, c[0], 255)
 
-    # get standard deviation of channel in Y plane
-    y_std = np.std(cv2.split(y_cb_cr_img)[0])
+    # Smooth mask and blur it
+    mask = cv2.dilate(mask, None, iterations=10)
+    mask = cv2.erode(mask, None, iterations=10)
+    mask = cv2.GaussianBlur(mask, (21, 21), 0)
+    # Create 3-channel alpha mask
+    mask_stack = np.dstack([mask] * 3)
+    # Blend mask and foreground image
+    mask_stack = mask_stack.astype("float32") / 255.0
+    img = img.astype("float32") / 255.0
+    masked = (mask_stack * img) + ((1 - mask_stack) * (1.0, 1.0, 1.0))
+    masked = (masked * 255).astype("uint8")
+    # Make the background transparent by adding 4th alpha channel
+    tmp = cv2.cvtColor(masked, cv2.COLOR_BGR2GRAY)
+    _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
+    b, g, r = cv2.split(masked)
+    rgba = [b, g, r, alpha]
 
-    # classify pixels as shadow and non-shadow pixels
-    for i in range(y_cb_cr_img.shape[0]):
-        for j in range(y_cb_cr_img.shape[1]):
-
-            if y_cb_cr_img[i, j, 0] < y_mean - (y_std / 3):
-                # paint it white (shadow)
-                binary_mask[i, j] = [255, 255, 255]
-            else:
-                # paint it black (non-shadow)
-                binary_mask[i, j] = [0, 0, 0]
-
-    # Using morphological operation
-    # The misclassified pixels are
-    # removed using dilation followed by erosion.
-    kernel = np.ones((3, 3), np.uint8)
-    erosion = cv2.erode(binary_mask, kernel, iterations=1)
-
-    # sum of pixel intensities in the lit areas
-    spi_la = 0
-
-    # sum of pixel intensities in the shadow
-    spi_s = 0
-
-    # number of pixels in the lit areas
-    n_la = 0
-
-    # number of pixels in the shadow
-    n_s = 0
-
-    # get sum of pixel intensities in the lit areas
-    # and sum of pixel intensities in the shadow
-    for i in range(y_cb_cr_img.shape[0]):
-        for j in range(y_cb_cr_img.shape[1]):
-            if (
-                erosion[i, j, 0] == 0
-                and erosion[i, j, 1] == 0
-                and erosion[i, j, 2] == 0
-            ):
-                spi_la = spi_la + y_cb_cr_img[i, j, 0]
-                n_la += 1
-            else:
-                spi_s = spi_s + y_cb_cr_img[i, j, 0]
-                n_s += 1
-
-    # get the average pixel intensities in the lit areas
-    average_ld = spi_la / n_la
-
-    # get the average pixel intensities in the shadow
-    average_le = spi_s / n_s
-
-    # difference of the pixel intensities in the shadow and lit areas
-    i_diff = average_ld - average_le
-
-    # get the ratio between average shadow pixels and average lit pixels
-    ratio_as_al = average_ld / average_le
-
-    # added these difference
-    for i in range(y_cb_cr_img.shape[0]):
-        for j in range(y_cb_cr_img.shape[1]):
-            if (
-                erosion[i, j, 0] == 255
-                and erosion[i, j, 1] == 255
-                and erosion[i, j, 2] == 255
-            ):
-
-                y_cb_cr_img[i, j] = [
-                    y_cb_cr_img[i, j, 0] + i_diff,
-                    y_cb_cr_img[i, j, 1] + ratio_as_al,
-                    y_cb_cr_img[i, j, 2] + ratio_as_al,
-                ]
-
-    # covert the YCbCr image to the BGR image
-    final_image = cv2.cvtColor(y_cb_cr_img, cv2.COLOR_YCR_CB2BGR)
-
-    cv2.imwrite("shadow_out.png", final_image)
+    return cv2.merge(rgba, 4)
 
 
-shadow_remover()
+# cv2.imwrite("shadow_out.png", background_removal(cv2.imread("shadow.png")))
