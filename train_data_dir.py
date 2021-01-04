02cbe9dae4d7a900857474a2ba6fe0b80460a77a
bluevariant <0x317374@gmail.com>
2021-01-04 14:41:56 +0700

    refactor(Drive): Detect drive.

--- train_data_dir.py
+++ train_data_dir.py
@@ -1,6 +1,23 @@
 from pathlib import Path
+from os.path import expanduser
 
-drive = "../drive/MyDrive/colab/lego_yolov5"
+home = expanduser("~")
 
-if not Path(drive).is_dir():
-    drive = "train_data"
+drive = "train_data"
+
+
+def init_drive():
+    global drive
+
+    dirs = [
+        "{}/Google Drive/colab/lego_yolov5".format(home),
+        "../drive/MyDrive/colab/lego_yolov5",
+    ]
+
+    for _dir in dirs:
+        if Path(_dir).is_dir():
+            drive = _dir
+            return
+
+
+init_drive()
